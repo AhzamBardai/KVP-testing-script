bebeeb48ce0eb42d22a2e6faa5beae2de63f6431
Ahzam <ahzam.bardai@outlook.com>
2021-11-11 18:48:47 -0600

    Better logs for processes

--- KVPTest.sh
+++ KVPTest.sh
@@ -1,6 +1,12 @@
 CMD=${1}
 
+echo "Removing testVue.txt (if exists)..."
+rm -f testVue.txt
+echo "Removal complete"
+
+echo "Performing Single File KVP check on file: $CMD"
 echo "$CMD" >> testVue.txt
-node index.js
-rm testVue.txt
+node singleFileKVPValuesTest.js
+
+echo "Completed"
 $SHELL
